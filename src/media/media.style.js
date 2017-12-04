6f586291543607ed770c2c58f538fe2f4c70bcc7
kmorales <kmorales@acsyt.com>
2017-12-04 10:57:57 -0600

    Modified Media style

--- src/media/media.style.js
+++ src/media/media.style.js
@@ -1,12 +1,15 @@
+import { Dimensions } from 'react-native'
+const WIDTH = Dimensions.get('window').width;
+
 /**
  * Compute style based on props
  * @param size The media size
  * @param hasRadius Does the media rounded or not ?
  * @param color The media color
  */
-export default ({ size = 40, hasRadius = false, color = '#efefef' }) => ({
-  height: size,
-  width: size,
+export default ({ size = 40, hasRadius = false, color = '#efefef', full = false }) => ({
+  height: full ? WIDTH * 0.95 : size,
+  width: full ? '100%' : size,
   borderRadius: hasRadius ? size / 2 : 3,
   backgroundColor: color,
 });
